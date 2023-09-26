5360b51288934ce46791e333c8d2d77a6ed1936b
Darlington <obii6776@gmail.com>
2023-09-26 08:56:02 +0100

    Added a new category

--- index.js
+++ index.js
@@ -36,17 +36,26 @@ if (document.title == "login"){
 }
 /* home.html */
 function handleFirstResponse(response){
-    const imagePath = response.results[3].poster_path;
-    const overview = response.results[3].overview;
+    const previewId = 81356;
+    let imageObj = [];
+    for (let i = 0; i < response.results.length; i++){
+        if (response.results[i].id == previewId){
+            imageObj = response.results[i];
+        }else{
+            continue;
+        }
+    }
+    const imagePath = imageObj.poster_path;
+    const overview = imageObj.overview;
     let firstParagraph = document.querySelector(".firstParagraph");
     let firstImage = document.querySelector(".firstImage");
     let trendingFlex = document.querySelector(".trending-flex");
    firstImage.src = `https://image.tmdb.org/t/p/w500${imagePath}`;
-   firstImage.alt = response.results[3].original_title
+   firstImage.alt = imageObj.original_title
    firstParagraph.innerText = overview;
    const results = response.results;
    for (let i = 0; i < results.length; i++){
-    if (i == 3){
+    if (response.results[i].id == 81356){
         continue;
     }
     let div = document.createElement("div");
@@ -56,6 +65,8 @@ function handleFirstResponse(response){
     div.append(image);
     trendingFlex.append(div);
    }
+    //console.log(previewId)
+    //console.log(imageObj);
 }
 async function fetchTrendingMovies(){
     const options = {
@@ -80,6 +91,8 @@ console.error(error);
 fetchTrendingMovies();
 
 async function fetchAll(){
+    let moviesAddedId = [];
+    let i = 0;
     const options = {
         method: 'GET',
         headers: {
@@ -91,28 +104,53 @@ async function fetchAll(){
       try{
         let newArray = [];
         let page = 1;
-        for (let i = 0; i < 20; i++){
+        for (let i = 0; i < 30; i++){
             const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`, options);
             const data = await response.json();
             newArray[i] = data.results;
             page+= 1;
         }
-        console.log(newArray);
+        //console.log(newArray);
         
         for (let a = 0; a < newArray.length; a++){
             for (let b = 0; b < newArray[a].length; b++){
-                if (newArray[a][b].genre_ids.includes(28) || newArray[a][b].genre_ids.includes(12)){
+                if (newArray[a][b].genre_ids.includes(28) && newArray[a][b].genre_ids.includes(12)){
                     let actionFlex = document.querySelector(".action-adventure-flex");
+                    moviesAddedId[i]  = newArray[a][b].id;
+                    i++
                     let div = document.createElement("div");
                     div.classList.add("trending-box");
                     let image = document.createElement("img");
                     image.src = `https://image.tmdb.org/t/p/w500${newArray[a][b].poster_path}`;
                     div.append(image);
                     actionFlex.append(div);
+                }else if(newArray[a][b].genre_ids.includes(16))
+                {
+                    let animationFlex = document.querySelector(".animation-flex");
+                    moviesAddedId[i]  = newArray[a][b].id;
+                    i++
+                    let div = document.createElement("div");
+                    div.classList.add("trending-box");
+                    let image = document.createElement("img");
+                    image.src = `https://image.tmdb.org/t/p/w500${newArray[a][b].poster_path}`;
+                    div.append(image);
+                    animationFlex.append(div);
+                }else if(newArray[a][b].genre_ids.includes(35) && newArray[a][b].genre_ids.includes(80))
+                {
+                    let crimeFlex = document.querySelector(".crime-flex");
+                    moviesAddedId[i]  = newArray[a][b].id;
+                    i++;
+                    let div = document.createElement("div");
+                    div.classList.add("trending-box");
+                    let image = document.createElement("img");
+                    image.src = `https://image.tmdb.org/t/p/w500${newArray[a][b].poster_path}`;
+                    div.append(image);
+                    crimeFlex.append(div);
                 }
                 
             }
         }
+        console.log(moviesAddedId);
     }catch (error){
             console.log(error);
           }
